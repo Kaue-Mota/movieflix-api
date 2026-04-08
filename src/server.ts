bd95ed7b3a0474d5eb461a3aa7e6b0ebba36aef8
Kaue-Mota <pckaue123@gmail.com>
2026-04-08 12:28:37 -0300

    "config"

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const port = 3000
+const app = express()
+const prisma = new PrismaClient()
+
+app.get('/movies', async (req, res) => {
+    const movies = await prisma.movies.findMany()
+    res.json(movies)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
